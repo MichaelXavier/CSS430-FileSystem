0eaa8c77914315de217f04a5a89b4356f8ed7c4b
Michael Xavier <michael@michaelxavier.net>
2010-03-04 00:59:36 -0800

    Most of the SysLib calls done.

--- SysLib.java
+++ SysLib.java
@@ -2,117 +2,177 @@ import java.util.*;
 
 public class SysLib {
     public static int exec( String args[] ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.EXEC, 0, args );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.EXEC, 0, args );
     }
 
     public static int join( ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.WAIT, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.WAIT, 0, null );
     }
 
     public static int boot( ) {
-	return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.BOOT, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+            Kernel.BOOT, 0, null );
     }
 
     public static int exit( ) {
-	return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.EXIT, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+            Kernel.EXIT, 0, null );
     }
 
     public static int sleep( int milliseconds ) {
-	return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.SLEEP, milliseconds, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+            Kernel.SLEEP, milliseconds, null );
     }
 
     public static int disk( ) {
-	return Kernel.interrupt( Kernel.INTERRUPT_DISK,
-				 0, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_DISK,
+              0, 0, null );
+    }
+
+    // NOTE: Added for Final Project
+    public static int format(int files) {
+      //FIXME: check logic
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.FORMAT, files, null );
+    }
+
+    // NOTE: Added for Final Project
+    public static int open(String fileName, String mode) {
+      //FIXME: check logic
+      //Put the strings into an array
+      String[] args = {fileName, mode};
+
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.OPEN, 0, args);
+    }
+
+    // NOTE: Added for Final Project
+    public static int read(int fd, byte[] buffer) {
+      //FIXME: check logic
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.READ, fd, buffer);
+    }
+
+    // NOTE: Added for Final Project
+    public static int write( int fd, byte[] buffer) {
+      //FIXME: check logic
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.WRITE, fd, buffer);
+    }
+
+    // NOTE: Added for Final Project
+    public static int seek( int fd, int offset, int whence ) {
+      //int isn't an object, should it be cast, should the last 2 be put into an arr?
+      //FIXME: cast needed? check logic
+      int[] args = {offset, whence};
+
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.SEEK, fd, args);
+    }
+
+    // NOTE: Added for Final Project
+    public static int close( int fd ) {
+      //FIXME: check logic
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.CLOSE, fd, null);
+    }
+
+    // NOTE: Added for Final Project
+    public static int delete(String fileName) {
+      //FIXME: check logic
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.DELETE, 0, fileName);
+    }
+
+    // NOTE: Added for Final Project
+    public static int fsize(int fd) {
+      //TODO: there's no const for this.
     }
 
     public static int cin( StringBuffer s ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.READ, 0, s );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.READ, 0, s );
     }
 
     public static int cout( String s ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.WRITE, 1, s );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.WRITE, 1, s );
     }
 
     public static int cerr( String s ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.WRITE, 2, s );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.WRITE, 2, s );
     }
 
     public static int rawread( int blkNumber, byte[] b ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.RAWREAD, blkNumber, b );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.RAWREAD, blkNumber, b );
     }
 
     public static int rawwrite( int blkNumber, byte[] b ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.RAWWRITE, blkNumber, b );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.RAWWRITE, blkNumber, b );
     }
 
     public static int sync( ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.SYNC, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.SYNC, 0, null );
     }
 
     public static int cread( int blkNumber, byte[] b ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.CREAD, blkNumber, b );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.CREAD, blkNumber, b );
     }
 
     public static int cwrite( int blkNumber, byte[] b ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.CWRITE, blkNumber, b );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.CWRITE, blkNumber, b );
     }
 
     public static int flush( ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.CFLUSH, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.CFLUSH, 0, null );
     }
 
     public static int csync( ) {
-        return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
-				 Kernel.CSYNC, 0, null );
+      return Kernel.interrupt( Kernel.INTERRUPT_SOFTWARE,
+        Kernel.CSYNC, 0, null );
     }
 
     public static String[] stringToArgs( String s ) {
-	StringTokenizer token = new StringTokenizer( s," " );
-	String[] progArgs = new String[ token.countTokens( ) ];
-	for ( int i = 0; token.hasMoreTokens( ); i++ ) {
-	    progArgs[i] = token.nextToken( );
-	}
-	return progArgs;
+      StringTokenizer token = new StringTokenizer( s," " );
+      String[] progArgs = new String[ token.countTokens( ) ];
+      for ( int i = 0; token.hasMoreTokens( ); i++ ) {
+          progArgs[i] = token.nextToken( );
+      }
+      return progArgs;
     }
 
     public static void short2bytes( short s, byte[] b, int offset ) {
-	b[offset] = (byte)( s >> 8 );
-	b[offset + 1] = (byte)s;
+      b[offset] = (byte)( s >> 8 );
+      b[offset + 1] = (byte)s;
     }
 
     public static short bytes2short( byte[] b, int offset ) {
-	short s = 0;
-        s += b[offset] & 0xff;
-	s <<= 8;
-        s += b[offset + 1] & 0xff;
-	return s;
+      short s = 0;
+            s += b[offset] & 0xff;
+      s <<= 8;
+            s += b[offset + 1] & 0xff;
+      return s;
     }
 
     public static void int2bytes( int i, byte[] b, int offset ) {
-	b[offset] = (byte)( i >> 24 );
-	b[offset + 1] = (byte)( i >> 16 );
-	b[offset + 2] = (byte)( i >> 8 );
-	b[offset + 3] = (byte)i;
+      b[offset] = (byte)( i >> 24 );
+      b[offset + 1] = (byte)( i >> 16 );
+      b[offset + 2] = (byte)( i >> 8 );
+      b[offset + 3] = (byte)i;
     }
 
     public static int bytes2int( byte[] b, int offset ) {
-	int n = ((b[offset] & 0xff) << 24) + ((b[offset+1] & 0xff) << 16) +
-	        ((b[offset+2] & 0xff) << 8) + (b[offset+3] & 0xff);
-	return n;
+      int n = ((b[offset] & 0xff) << 24) + ((b[offset+1] & 0xff) << 16) +
+              ((b[offset+2] & 0xff) << 8) + (b[offset+3] & 0xff);
+      return n;
     }
 }
